cd2fc26688d923668509c0bd046d828aa8545ca9
Zhang, Shulai <Shulai.Zhang@sungard.com>
2015-01-30 11:26:35 +0800

    updating

---
+++ src/main/java/org/ikgroup/service/activity/behavior/TestActivityBehavior.java
@@ -0,0 +1,14 @@
+package org.ikgroup.service.activity.behavior;
+
+import org.activiti.engine.impl.pvm.delegate.ActivityBehavior;
+import org.activiti.engine.impl.pvm.delegate.ActivityExecution;
+
+public class TestActivityBehavior implements ActivityBehavior {
+
+	@Override
+	public void execute(ActivityExecution execution) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
